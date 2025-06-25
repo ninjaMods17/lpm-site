934438968bedace774c9e247543de30cbac07f0f
Ninja Shadow <ninjashshadow13@gmail.com>
2025-06-26 00:09:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.remove('active');
+  });
+});
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
